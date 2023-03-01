fd150489ca9a1972365c2fa8ffa8cbf0212dbbbf
engArthurFranca <eng.arthurfranca+github@gmail.com>
2023-03-01 00:31:05 -0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,12 @@
 set -e
 # build
 npm run build
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
 git init
-git add dist
+git add -A
 git commit -m 'deploy'
 git push -f git@github.com:engArthurFranca/cryptogram-vue3.git main:gh-pages
+cd -
